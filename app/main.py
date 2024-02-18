afacf2cde36654beb85ede73939c8cd049ad4b8e
jankoscialkowski <jan.koscialkowski@gmail.com>
2024-02-18 23:26:47 +0100

    feat: Add Prometheus

--- app/main.py
+++ app/main.py
@@ -1,13 +1,42 @@
 from random import random
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+from prometheus_client import Counter, Summary, make_asgi_app
 
 app = FastAPI()
+
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
+
+c_total_requests = Counter("requests", "Total number of requests")
+c_failed_requests = Counter("failed_requests", "Number of failed requests", ["status_code"])
+c_failed_requests.labels("418")
+c_failed_requests.labels("420")
+s_latency = Summary("request_latency_seconds", "Request latency in seconds")
 
 
 @app.get("/")
 async def root():
-    if random() < 0.1:
+    number = random()
+    if number < 0.001:
+        raise HTTPException(status_code=420, detail="Blaze it")
+    if number < 0.1:
         raise HTTPException(status_code=418, detail="I'm a teapot")
 
-    return {"message": "Hello World"}
+    return {"message": "OK!"}
+
+
+@app.middleware("http")
+async def log_metrics(request: Request, call_next):
+    with s_latency.time():
+        c_total_requests.inc()
+        response = await call_next(request)
+
+    return response
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    c_failed_requests.labels(status_code=str(exc.status_code)).inc()
+    return JSONResponse({"detail": exc.detail}, status_code=exc.status_code)
